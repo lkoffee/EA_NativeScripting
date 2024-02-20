b473032574235e8c292b941477a5a16dd2265ecc
Diego Matos <lambdakoffee@gmail.com>
2024-02-20 02:34:10 +0000

    Just to check versions and node tip

---
+++ mycheck.sh
@@ -0,0 +1,10 @@
+
+echo "[cardano-cli --version]"
+cardano-cli --version
+echo ""
+echo "[cardano-node --version]"
+cardano-node --version
+echo ""
+echo "[cardano-cli query tip]" 
+cardano-cli query tip --testnet-magic 2
+
