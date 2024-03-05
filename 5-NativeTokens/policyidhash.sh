ff0fedd8b9c70282712acc9c76d1df76dd48f446
Diego Matos <lambdakoffee@gmail.com>
2024-03-05 15:19:14 +0000

    update policyIdHash

--- 5-NativeTokens/policyidhash.sh
+++ 5-NativeTokens/policyidhash.sh
@@ -2,4 +2,6 @@ echo "Generating Policy id Hash:"
 # Calculate the PolicyId from the monetary policy script.
 cardano-cli transaction policyid \
     --script-file mypolicy.script \
-    > mymonetary2.pid    
+    > mymonetary2.pid
+
+cat ./mymonetary2.pid    
