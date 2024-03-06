3a79208643c000820508c83bd70da40abee51e8f
Diego Matos <lambdakoffee@gmail.com>
2024-03-06 06:01:06 +0000

    5-Native Tokens - new files

--- 5-NativeTokens/mysminting.sh
+++ 5-NativeTokens/mysminting.sh
@@ -1,45 +1,36 @@
 #EADR1 UTXO:
-utxoin=56c35504f77d77e6f092d2bb303503db84c8211bfa4f04986f04b08f28420b9c#1
-policyid=$(cat mymonetary2.pid)
-address=addr_test1vzxm33pyftx0fefr8g7e3s9rtd44cwzeqenddyuc5ej30qgf6duvh
+utxoin=ba030dbd39cfdcfc281a1fe504d5603be35eb170b42a78ece46e90c659e55569#1
+eadr1=addr_test1vzxm33pyftx0fefr8g7e3s9rtd44cwzeqenddyuc5ej30qgf6duvh
 output=20000000
-tokenname=$(echo -n "Diego Coffee Coin" | xxd -ps | tr -d '\n')
-#Suply
-tokenammount=1000000
+policyid=$(cat mymonetary3.pid)
+tokenname=$(echo -n "Diego+Coffee+Coin" | xxd -ps | tr -d '\n')
 #
-mintammount=1000000000
+tokenammount=1000
+#
+mintammount=10000
+# For Collateral (UTXO and the EADR2 Pub-Key-Hash)
 #EADR2 UTXO addr_test1vrr4fp9q4vk5mfs3f2x2gfgqef73qq8qdvp8ve0uwr0g94gsr7qk0:
 collateral=3196a2de0a4420004aa88fbb9788d41ecec12f0a3b68372023a21de056055f44#1
-# Hash of EADR1 vkey:
-signerPKH1=8db8c4244accf4e5233a3d98c0a35b6b5c38590666d69398a6651781
-#          8db8c4244accf4e5233a3d98c0a35b6b5c38590666d69398a6651781 (Payment Credential)
-# Hash of EADR2 vkey:
-signerPKH2=c75484a0ab2d4da6114a8ca42500ca7d1000e06b027665fc70de82d5
+signerPKH=c75484a0ab2d4da6114a8ca42500ca7d1000e06b027665fc70de82d5
 #          c75484a0ab2d4da6114a8ca42500ca7d1000e06b027665fc70de82d5 (Payment Credential)
-
-# nami=addr_test1qpc6mrwu9cucrq4w6y69qchflvypq76a47ylvjvm2wph4szeq579yu2z8s4m4tn0a9g4gfce50p25afc24knsf6pj96sz35wnt
+# Hash of EADR1 vkey:
+# signerPKH1=8db8c4244accf4e5233a3d98c0a35b6b5c38590666d69398a6651781
+# #          8db8c4244accf4e5233a3d98c0a35b6b5c38590666d69398a6651781 (Payment Credential)
+nami=addr_test1qpc6mrwu9cucrq4w6y69qchflvypq76a47ylvjvm2wph4szeq579yu2z8s4m4tn0a9g4gfce50p25afc24knsf6pj96sz35wnt
 # cardano-cli query protocol-parameters --testnet-magic 2 --out-file protocol.params
 
 cardano-cli transaction build \
   --babbage-era \
   --testnet-magic 2 \
   --tx-in $utxoin \
-  --required-signer-hash $signerPKH1 \
-  --required-signer-hash $signerPKH2 \
+  --required-signer-hash $signerPKH \
   --tx-in-collateral $collateral \
-  --tx-out $address+$output+"$tokenammount $policyid.$tokenname" \
-  --change-address $address \
+  --tx-out $nami+$output+"$tokenammount $policyid.$tokenname" \
+  --change-address $eadr1 \
   --mint "$mintammount $policyid.$tokenname" \
   --mint-script-file mypolicy.script \
-  --invalid-hereafter 42956000 \
+  --invalid-hereafter 43039000 \
   --out-file sminting.unsigned
-
-# "slot": 42948317,
-# Estimated transaction fee: Lovelace 191681
-# Transaction successfully submitted.
-
-# --tx-out "$address $output + $tokenammount $policyid.$tokenname" \
-# --tx-out "$address + $output + $tokenammount $policyid.$tokenname" \
 
 cardano-cli transaction sign \
   --tx-body-file sminting.unsigned \
